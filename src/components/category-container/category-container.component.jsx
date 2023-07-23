62fead78ee55297678de484a8744d390113fc398
Joel Carlo M. Menor <menorjc@live.com>
2023-07-23 22:25:04 +0800

    Navigation and logo

--- src/components/category-container/category-container.component.jsx
+++ src/components/category-container/category-container.component.jsx
@@ -1,9 +1,7 @@
 import './category-container.styles.scss';
-
-import CategoryModel from '../category-model/category-model.component';
 import CategoryItem from '../category-item/category-item.component';
 
-const CategoryContainer = () => {
+const CategoryContainer = ({CategoryModel}) => {
     return (
         <div className="categories-container">
             {CategoryModel.map((category) => {
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,8 @@
+import CategoryContainer from '../../components/category-container/category-container.component';
+import CategoryModel from '../../components/category-model/category-model.component';
+
+const Home = () =>  {
+    return <CategoryContainer CategoryModel={CategoryModel} />; 
+}
+
+export default Home;
